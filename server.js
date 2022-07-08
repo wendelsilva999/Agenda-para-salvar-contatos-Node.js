6286eca5338d253c174c1f1836a20dbfa6296615
wendel átila silva <104469175+wendelsilva999@users.noreply.github.com>
2022-07-08 07:28:34 -0300

    update

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(routes)
 
 
 app.on('pronto', () => {
-    app.listen(3000, () => {
+    app.listen(process.env.APP_URL, () => {
         console.log('Acessar http://localhost:3000')
         console.log('servidor executando na porta 3000')
     });
